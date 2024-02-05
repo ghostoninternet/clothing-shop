c80864d33e4de742cc2b31ff561c0f168ac3fe81
hiewbt <hieubt.bk66@gmail.com>
2024-02-05 21:55:40 +0700

    Update header, routing

--- clothing-shop-client/src/routes/index.jsx
+++ clothing-shop-client/src/routes/index.jsx
@@ -3,12 +3,16 @@ import { HeaderOnly } from '../components/Layout'
 import Home from '../pages/Home'
 import Nam from '../pages/Nam'
 import Nu from '../pages/Nu'
+import Kid from '../pages/Kid'
+import Baby from '../pages/Baby'
 
 
 const publicRoutes = [
     { path: '/', component: Home, layout: HeaderOnly},
-    { path: '/nam', component: Nam},
-    { path: '/nu', component: Nu},
+    { path: '/men', component: Nam},
+    { path: '/women', component: Nu},
+    { path: '/kid', component: Kid},
+    { path: '/baby', component: Baby},
 ]
 
 const privateRoutes = [
