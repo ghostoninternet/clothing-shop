68e598e75aa5d564350fec346aea0ea40a08c19c
hiewbt <hieubt.bk66@gmail.com>
2024-02-21 00:02:47 +0700

    add admin page

---
+++ clothing-shop-client/src/pages/Admin/components/Product/index.jsx
@@ -0,0 +1,12 @@
+import classNames from 'classnames/bind'
+import styles from './Product.module.scss'
+
+const cx = classNames.bind(styles)
+
+function Product() {
+    return ( 
+        <div className="">Product Component</div>
+    )
+}
+
+export default Product
